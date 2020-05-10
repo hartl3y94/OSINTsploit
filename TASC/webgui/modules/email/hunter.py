d69be884d5fdf28b9e8e9c7a6123b596fda53325
adithyan-ak <adithyanhaxor@gmail.com>
2020-05-10 22:26:48 +0530

    Restructured Hunter.io

--- TASC/webgui/modules/email/hunter.py
+++ TASC/webgui/modules/email/hunter.py
@@ -4,13 +4,12 @@ def hunter(email,apikey):
     url="https://api.hunter.io/v2/email-verifier?email="+email+"&api_key="+apikey
     response=requests.get(url)
     if response.status_code==200:
-        data=[]
-        data=response.json()['data']['sources']
-        data=[dict(t) for t in {tuple(d.items()) for d in data}]
+        hunterdata={}
+        hunterdata=response.json()
         
-        if len(data) == 0:
+        if len(hunterdata) == 0:
             return {'Error':'The Email is Not Breached'}
         else:
-            return data
+            return hunterdata
     else:
         return {'Error':'Something Went Wrong'}
