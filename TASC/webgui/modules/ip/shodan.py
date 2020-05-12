2c6fbd3f5cd4cdfaf246b87a824030a787cb7614
aravindha1234u <aravindha1234u@gmail.com>
2020-05-12 19:32:41 +0530

    fixed shodan

--- TASC/webgui/modules/ip/shodan.py
+++ TASC/webgui/modules/ip/shodan.py
@@ -3,27 +3,29 @@ import json
 
 def shodan_ip(IP,apikey):
     shodan={}
+    shodan['honeypot']=honeypot(IP,apikey)
     url="https://api.shodan.io/shodan/host/"+IP+"?key="+apikey
     try:
         response=requests.get(url)
         js=json.loads(response.text)
         shodan['host']=js
-        shodan['honeypot']=honeypot(IP,apikey)
+        return shodan
     except:
         return {'Error':'Something Went Wrong'}
     
 def honeypot(ip,apikey):
+    result={}
     honey = "https://api.shodan.io/labs/honeyscore/"+ip+"?key="+apikey
     try:
-        result = requests.get(honey).json
+        probability= requests.get(honey).text
+        if ("error" in result) or ("404" in result):
+            result['Error'] = 'IP Not Found'
+        else:
+            probability = str(float(probability) * 10)
+            result['HoneyPot Probability']=probability 
+            return result
     except:
-        result['Message'] ='No information Found'
-
-    if "error" in result or "404" in result:
-        result['Error'] = 'IP Not Found'
-    elif result:
-        probability = str(float(result) * 10)
-        result['HoneyPot Probability']=probability
+        result['Error']='No Information Found'
     else:
         result['Error']='Something Went Wrong'
     return result
