ccfda9a1a0ebe82fe4d674ce0e41667a576f15df
adithyan-ak <adithyanhaxor@gmail.com>
2020-05-17 20:30:33 +0530

    Removed JSONfield requirement

--- TASC/webgui/models.py
+++ TASC/webgui/models.py
@@ -3,7 +3,6 @@ from django.db.models import Model
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from jsonfield import JSONField
 
 
 class Profile(models.Model):
