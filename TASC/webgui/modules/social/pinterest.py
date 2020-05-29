8da2de261d599c54c9b35c4370a56532286fd2b9
aravindha1234u <aravindha1234u@gmail.com>
2020-05-29 17:38:58 +0530

    Added Medium, Pinterset,Tiktok scraper into social

---
+++ TASC/webgui/modules/social/pinterest.py
@@ -0,0 +1,81 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import json
+
+def pinterest(username):
+    session = requests.session()
+    user_agent_list = [
+        #Chrome
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+            #Firefox
+            'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+            'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+            'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+        ]
+
+    headers = {
+        'User-Agent': user_agent_list[random.randint(0,len(user_agent_list)-1)],
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'TE': 'Trailers',
+    }
+
+    session.headers=headers
+    session.proxies = {'http':  'socks5://127.0.0.1:9050','https': 'socks5://127.0.0.1:9050'}
+    #print(session.get("http://httpbin.org/ip").text)
+    #print(session.get("https://httpbin.org/user-agent").text)
+    
+    url="https://in.pinterest.com/"+username+"/"
+    
+    response=session.get(url,verify=False)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content,'html.parser')
+        script_tag = soup.find('script',{'id' : 'initial-state'})
+        json_data = json.loads(str(script_tag.text.strip()))
+        data = json_data['resourceResponses'][0]['response']['data']
+        user_data = data['user']
+            
+        is_verified_merchant = user_data['is_verified_merchant']
+        full_name = user_data['full_name']
+        impressum_url = user_data['impressum_url']
+        pin_count = user_data['pin_count']
+        domain_url = user_data['domain_url']
+        profile_image = user_data['image_xlarge_url']
+        bio = user_data['about']
+        board_count = user_data['board_count']
+        is_indexed = user_data['indexed']
+        follower = user_data['follower_count']
+        following = user_data['following_count']
+        country = user_data['country']
+        location = user_data['location']
+            
+        return{'full_name' : full_name,'profile_image' : profile_image,'followers' : follower,'followings' : following,'bio' : bio,
+    'country' : country,'impressum_url' : impressum_url,'website' : domain_url,'board_count' : board_count,'location' : location,
+    'pin_count' : pin_count,'is_verified' : is_verified_merchant,}
