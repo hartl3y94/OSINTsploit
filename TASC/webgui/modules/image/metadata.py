59c2af9f98f3827bb40a1a3a7d4d27760b4aa59e
Aravindha1234u <aravindha1234u@gmail.com>
2020-07-21 23:05:33 +0530

    Metadata Finsihed

--- TASC/webgui/modules/image/metadata.py
+++ TASC/webgui/modules/image/metadata.py
@@ -5,17 +5,19 @@ from django.contrib.auth.models import User, auth
 import os
 from GPSPhoto import gpsphoto
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from xhtml2pdf import pisa
+from pyvirtualdisplay import Display
+from django.shortcuts import render, redirect
+import pdfx, pdfkit, urllib.parse, urllib3, tempfile
+from ..social.locmap import loc, heat_map, gps_map
 
-def get_exif(metaimage):
-
-    user.profile.metaimage = metaimage
-    user.profile.save()
+def get_exif(url): 
     metadata = {}
-    metaimage = BASE_DIR + user.profile.metaimage.url
+    metaimage = BASE_DIR + url
     data = gpsphoto.getGPSData(metaimage)
     i = Image.open(metaimage)
     info = i.getexif()
-    os.remove(BASE_DIR + user.profile.metaimage.url)
+    os.remove(BASE_DIR + url)
     if info == {}:
         error = {'Error':'No Metadata Found'}
         metadata = {}
@@ -38,7 +40,43 @@ def get_exif(metaimage):
 
         return metadata
 
-   
-
     # maps_url = "https://maps.google.com/maps?q=%s,+%s" % (lat, lon)
-  
+def MetaPdf(request,url):
+    pdf=pdfx.PDFx(BASE_DIR + url)
+    os.remove(BASE_DIR + url)
+    metadata=pdf.get_metadata()
+    if pdf.get_references_as_dict() != {}:
+        metadata['references_dict'] = pdf.get_references_as_dict()
+    return render(request, 'apps/metadata.html',{'metadata':metadata, "POST":"post"})
+    
+def Metadata(request):
+    username = request.user.username
+    user = User.objects.filter(username=username).first()
+    user.profile.metaimage = request.FILES['metaimage']
+    user.profile.save()
+    googlemapapikey = user.profile.googlemapapikey
+    filename=str(request.FILES['metaimage']).split('.',1)
+
+    if filename[-1] in ['jpg','png','gif','tif','jpeg']:
+
+        metadata = get_exif(user.profile.metaimage.url)
+
+        if 'Error' in metadata.keys():
+            return render(request, 'apps/metadata.html',{'metadata':metadata, "POST":"post"})
+
+        elif 'Latitude' in metadata.keys():
+
+            lats = metadata['Latitude']
+            lons = metadata['Longitude']
+            gmap3=heat_map([lats],[lons], googlemapapikey)
+            return render(request, 'apps/metadata.html',{'metadata':metadata, 'gmap3':gmap3, "POST":"post"})
+
+        else:
+            return render(request, 'apps/metadata.html',{'metadata':metadata, "POST":"post"})
+
+    elif filename[-1] == 'pdf':
+        return MetaPdf(request,user.profile.metaimage.url)
+    else:
+        return render(request, 'apps/metadata.html',{"Error":"Upload a filename with Valid Extension"})
+
+    
