03ed81c0efd736fced46ca402c12fcec1cda0586
adithyan-ak <adithyanhaxor@gmail.com>
2020-07-28 14:22:57 +0530

    Notifications added to base.html

--- TASC/webgui/templatetags/extras.py
+++ TASC/webgui/templatetags/extras.py
@@ -1,6 +1,7 @@
 import json, yaml
-
+from django.contrib.auth.models import User
 from django import template
+from ..modules.filehandlers import HistoryData
 
 register = template.Library()
 
@@ -10,6 +11,19 @@ register = template.Library()
 def pretty_json(value):
 
     return yaml.dump(value, default_flow_style=False)
+
+@register.filter
+
+def getnotification(value):
+    username = value
+    print(username)
+    try:
+        history = HistoryData("media/json/history_{}.json".format(username),"r")
+    except FileNotFoundError:
+        history = HistoryData("media/json/history_{}.json".format(username),"w",open("templates/json/history.json").read())
+        
+    notifications = history['notifications'][:5]
+    return notifications
 
 
 @register.filter
